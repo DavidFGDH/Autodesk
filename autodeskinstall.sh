2f1b721a596cd1829d40778de8250d654ff171ef
DavidFGDH <32119775+DavidFGDH@users.noreply.github.com>
2022-02-16 23:00:38 +0000

    Add files via upload

---
+++ autodeskinstall.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install htop
+yes '' | sudo apt-add-repository ppa:remmina-ppa-team/remmina-next
+sudo apt update
+sudo apt install remmina remmina-plugin-rdp remmina-plugin-secret -y
+wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/97.0/linux-x86_64/es-ES/firefox-97.0.tar.bz2
+tar -jxvf firefox-97.0.tar.bz2
